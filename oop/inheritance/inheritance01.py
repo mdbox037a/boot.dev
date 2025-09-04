f0b439a5b52002a81c0aa091819883593339fcba
matt box <mdbox037a@protonmail.com>
2025-09-04 15:28:55 -0400

    crete inheritance01.py skeleton

---
+++ oop/inheritance/inheritance01.py
@@ -0,0 +1,17 @@
+class Human:
+    def __init__(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+
+## don't touch above this line
+
+
+class Archer:
+    def __init__(self, name, num_arrows):
+        pass
+
+    def get_num_arrows(self):
+        pass
