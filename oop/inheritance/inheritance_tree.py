9f0a752a5a70ca33e9bd2c56d856a461d6acc241
matt box <mdbox037a@protonmail.com>
2025-09-06 15:00:50 -0400

    create multiple children inheritance skeleton

---
+++ oop/inheritance/inheritance_tree.py
@@ -0,0 +1,24 @@
+class Hero:
+    def __init__(self, name, health):
+        self.__name = name
+        self.__health = health
+
+    def get_name(self):
+        return self.__name
+
+    def get_health(self):
+        return self.__health
+
+    def take_damage(self, damage):
+        self.__health -= damage
+
+
+# don't touch above this line
+
+
+class Archer:
+    def __init__(self, name, health, num_arrows):
+        pass
+
+    def shoot(self, target):
+        pass
