c3a1e4568cf0a3230be028c527ee7a794693e668
matt box <mdbox037a@protonmail.com>
2025-09-07 17:25:48 -0400

    add armyFormations skeleton script

---
+++ oop/inheritance/armyFormations.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, length, width):
+        pass
+
+    def get_area(self):
+        pass
+
+    def get_perimeter(self):
+        pass
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        pass
