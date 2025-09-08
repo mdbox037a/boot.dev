f82f1ca1051c7331f0b212a9b36b916574ac3c93
matt box <mdbox037a@protonmail.com>
2025-09-07 22:09:16 -0400

    complete overlap logic

--- oop/inheritance/overlap.py
+++ oop/inheritance/overlap.py
@@ -1,6 +1,18 @@
 class Rectangle:
     def overlaps(self, rect):
-        pass
+        # Check for overlap on the x-axis
+        x_overlap = (
+            self.get_right_x() >= rect.get_left_x()
+            and self.get_left_x() <= rect.get_right_x()
+        )
+
+        # Check for overlap on the y-axis
+        y_overlap = (
+            self.get_top_y() >= rect.get_bottom_y()
+            and self.get_bottom_y() <= rect.get_top_y()
+        )
+
+        return x_overlap and y_overlap
 
     # don't touch below this line
 
