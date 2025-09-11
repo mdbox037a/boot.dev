d64a9ea8118087456662e56168ac53b863d9ca3e
matt box <mdbox037a@protonmail.com>
2025-09-10 22:49:46 -0400

    complete __add__ override

--- oop/polymorphism/type_overloading.py
+++ oop/polymorphism/type_overloading.py
@@ -1,3 +1,14 @@
 class Sword:
     def __init__(self, sword_type):
         self.sword_type = sword_type
+
+    def __add__(self, other):
+        sword1 = self.sword_type
+        sword2 = other.sword_type
+
+        if sword1 == "bronze" and sword2 == "bronze":
+            return Sword("iron")
+        elif sword1 == "iron" and sword2 == "iron":
+            return Sword("steel")
+        else:
+            raise Exception("cannot craft")
