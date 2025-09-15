37422501696a22c5958ec0529eb203dce08769d0
matt box <mdbox037a@protonmail.com>
2025-09-14 21:35:03 -0400

    create high/low card game skeleton

---
+++ oop/polymorphism/high_card.py
@@ -0,0 +1,44 @@
+SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
+RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.rank_index = RANKS.index(rank)
+        self.suit_index = SUITS.index(suit)
+
+    def __eq__(self, other):
+        return (
+            self.rank_index == other.rank_index and self.suit_index == other.suit_index
+        )
+
+    def __lt__(self, other):
+        if self.rank_index == other.rank_index:
+            return self.suit_index < other.suit_index
+        return self.rank_index < other.rank_index
+
+    def __gt__(self, other):
+        if self.rank_index == other.rank_index:
+            return self.suit_index > other.suit_index
+        return self.rank_index > other.rank_index
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+
+class Round:
+    def resolve_round(self):
+        raise NotImplementedError("Subclasses must implement resolve_round()")
+
+
+# Don't touch above this line
+
+
+class HighCardRound(Round):
+    pass
+
+
+class LowCardRound(Round):
+    pass
