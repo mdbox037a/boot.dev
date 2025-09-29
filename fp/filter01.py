3b4eb3b686f46fe9053471b542aa468b98c310cc
matt box <mdbox037a@protonmail.com>
2025-09-28 21:29:00 -0400

    complete logic for filter+lambda example

--- fp/filter01.py
+++ fp/filter01.py
@@ -1,2 +1,4 @@
 def remove_invalid_lines(document):
-    pass
+    lines_list = document.split("\n")
+    filtered_list = filter(lambda line: not line.startswith("-"), lines_list)
+    return "\n".join(filtered_list)
