eb7af1d4edb76b0f3c6269deb2b5e356266d709c
matt box <mdbox037a@protonmail.com>
2025-09-29 15:01:16 -0400

    complete logic for functools.reduce example

--- fp/reduce01.py
+++ fp/reduce01.py
@@ -2,8 +2,13 @@ import functools
 
 
 def join(doc_so_far, sentence):
-    pass
+    return doc_so_far + ". " + sentence
 
 
 def join_first_sentences(sentences, n):
-    pass
+    if n == 0:
+        return ""
+    else:
+        sentence_list = sentences[0:n]
+        joined = functools.reduce(join, sentence_list)
+        return joined + "."
