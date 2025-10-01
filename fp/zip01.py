8d2be933d26bdcbc199e158909c7dc5b4e6ecac4
matt box <mdbox037a@protonmail.com>
2025-09-30 20:56:54 -0400

    create first zip function exercise and test script

---
+++ fp/zip01.py
@@ -0,0 +1,19 @@
+valid_formats = [
+    "docx",
+    "pdf",
+    "txt",
+    "pptx",
+    "ppt",
+    "md",
+]
+
+# Don't edit above this line
+
+
+def pair_document_with_format(doc_names, doc_formats):
+    raw_pairs = list(zip(doc_names, doc_formats))
+    final_pairs = []
+    for pair in raw_pairs:
+        if pair[1] in valid_formats:
+            final_pairs.append(pair)
+    return final_pairs
