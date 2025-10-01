a42cd51366222f90bb166f5d40bc6e078c4ec7fe
matt box <mdbox037a@protonmail.com>
2025-09-30 21:00:39 -0400

    refactor pair function to simplify with filter() and lambda

--- fp/zip01.py
+++ fp/zip01.py
@@ -11,9 +11,12 @@ valid_formats = [
 
 
 def pair_document_with_format(doc_names, doc_formats):
+    # raw_pairs = list(zip(doc_names, doc_formats))
+    # final_pairs = []
+    # for pair in raw_pairs:
+    #    if pair[1] in valid_formats:
+    #        final_pairs.append(pair)
+    # return final_pairs
+
     raw_pairs = list(zip(doc_names, doc_formats))
-    final_pairs = []
-    for pair in raw_pairs:
-        if pair[1] in valid_formats:
-            final_pairs.append(pair)
-    return final_pairs
+    return list(filter(lambda pair: pair[1] in valid_formats, raw_pairs))
