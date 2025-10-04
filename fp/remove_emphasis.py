d096581e38fbec2df5550fd709fce0f751a6e274
matt box <mdbox037a@protonmail.com>
2025-10-04 17:09:56 -0400

    complete function chain to clean lines of emphasis

--- fp/remove_emphasis.py
+++ fp/remove_emphasis.py
@@ -1,10 +1,16 @@
 def remove_emphasis_from_word(word):
-    pass
+    return word.strip("*")
 
 
 def remove_emphasis_from_line(line):
-    pass
+    function_line = line
+    words = function_line.split()
+    clean_words = map(remove_emphasis_from_word, words)
+    return " ".join(clean_words)
 
 
 def remove_emphasis(doc_content):
-    pass
+    function_content = doc_content
+    lines = function_content.split("\n")
+    clean_doc = "\n".join(map(remove_emphasis_from_line, lines))
+    return clean_doc
