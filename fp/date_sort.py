d80992c9f77f373869c3f0400c0d321170bbc26c
matt box <mdbox037a@protonmail.com>
2025-10-05 17:57:33 -0400

    add more streamlined solution and feel bad about myself

--- fp/date_sort.py
+++ fp/date_sort.py
@@ -3,3 +3,15 @@ def sort_dates(dates):
     md_array = list(map(lambda x: x.split("-"), working_list))
     sorted_md_array = sorted(md_array, key=lambda x: (x[2], x[0], x[1]))
     return list(map(lambda x: "-".join(x), sorted_md_array))
+
+
+# the following would have been a lot better...
+
+
+def better_sort_dates(dates):
+    return sorted(dates, key=format_date)
+
+
+def format_date(date):
+    month, day, year = date.split("-")
+    return year + month + day
