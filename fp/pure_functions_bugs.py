9952d3efc8702256b8fe858ba6428f2ecf1a162a
matt box <mdbox037a@protonmail.com>
2025-10-05 16:51:37 -0400

    complete edits to make all functions pure functions

--- fp/pure_functions_bugs.py
+++ fp/pure_functions_bugs.py
@@ -6,19 +6,22 @@ saved_documents = {}
 
 
 def add_custom_command(commands, new_command, function):
-    commands[new_command] = function
-    return commands
+    updated_commands = commands.copy()
+    updated_commands[new_command] = function
+    return updated_commands
 
 
 def add_format(formats, format):
-    formats.append(format)
-    return formats
+    updated_formats = formats.copy()
+    updated_formats.append(format)
+    return updated_formats
 
 
 def save_document(docs, file_name, doc):
-    docs[file_name] = doc
-    return docs
+    updated_docs = docs.copy()
+    updated_docs[file_name] = doc
+    return updated_docs
 
 
 def add_line_break(line):
-    print(line + "\n\n")
+    return line + "\n\n"
