5321bd9a532d6d398ddea43826d9bc7c89cb1535
matt box <mdbox037a@protonmail.com>
2025-10-05 16:42:19 -0400

    complete first memoization script exercisesdf

--- fp/memozation01.py
+++ fp/memozation01.py
@@ -1,5 +1,10 @@
 def word_count_memo(document, memos):
-    pass
+    updated_memos = memos.copy()
+    if document in updated_memos:
+        return updated_memos[document], updated_memos
+    else:
+        updated_memos[document] = word_count(document)
+        return word_count(document), updated_memos
 
 
 # Don't edit below this line
