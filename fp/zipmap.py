ce94fcf7b7b4419dabad677e2e3548d8d7fa4f4d
matt box <mdbox037a@protonmail.com>
2025-10-06 16:21:48 -0400

    create first version of zipmap solution

--- fp/zipmap.py
+++ fp/zipmap.py
@@ -1,2 +1,7 @@
 def zipmap(keys, values):
-    pass
+    if len(keys) == 0 or len(values) == 0:
+        zipped = {}
+        return zipped
+    zipped = zipmap(keys[1:], values[1:])
+    zipped = {keys[0]: values[0], **zipped}
+    return zipped
