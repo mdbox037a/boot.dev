99d1475da263fdcfcaec18ec207eaf7ea03f7fc9
matt box <mdbox037a@protonmail.com>
2025-10-06 15:05:01 -0400

    add example comment to show how to test functions from python3 console

--- fp/sum01.py
+++ fp/sum01.py
@@ -2,3 +2,7 @@ def sum_nums(nums):
     if len(nums) == 0:
         return 0
     return nums[0] + sum_nums(nums[1:])
+
+
+# call from terminal for testing:
+# python3 -c "from sum01 import sum_nums; print(sum_nums((1, 2, 3)))"
