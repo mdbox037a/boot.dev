cc68776288ad21048674dcf732552392b4c9d1b4
matt box <mdbox037a@protonmail.com>
2025-10-06 16:23:43 -0400

    streamline code a bit: empty dict in one line, and don't care about dictionary order for equality

--- fp/zipmap.py
+++ fp/zipmap.py
@@ -1,7 +1,6 @@
 def zipmap(keys, values):
     if len(keys) == 0 or len(values) == 0:
-        zipped = {}
-        return zipped
+        return {}
     zipped = zipmap(keys[1:], values[1:])
-    zipped = {keys[0]: values[0], **zipped}
+    zipped[keys[0]] = values[0]
     return zipped
