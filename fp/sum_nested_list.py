cd90ff418d7fd388f6bae91f4345179dbb36f7c6
matt box <mdbox037a@protonmail.com>
2025-10-07 15:10:46 -0400

    complete recursive logic for tree total function

--- fp/sum_nested_list.py
+++ fp/sum_nested_list.py
@@ -1,2 +1,10 @@
 def sum_nested_list(lst):
-    pass
+    total_size = 0
+    for item in lst:
+        if isinstance(item, int):
+            total_size += item
+        elif isinstance(item, list):
+            total_size += sum_nested_list(item)
+        else:
+            return -1
+    return total_size
