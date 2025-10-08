c0d889d456b05c67d6a0c8abdec1cb141c9545a7
matt box <mdbox037a@protonmail.com>
2025-10-08 16:58:57 -0400

    complete recursive filesystem lister

--- fp/list_files.py
+++ fp/list_files.py
@@ -1,2 +1,10 @@
 def list_files(parent_directory, current_filepath=""):
-    pass
+    print(parent_directory, current_filepath)
+    paths = []
+    for key in parent_directory:
+        new_filepath = current_filepath + "/" + key
+        if parent_directory[key] is None:
+            paths.append(new_filepath)
+        else:
+            paths.extend(list_files(parent_directory[key], new_filepath))
+    return paths
