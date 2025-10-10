558fcffca4452bc233a200d35edf494896aaf3e9
matt box <mdbox037a@protonmail.com>
2025-10-09 21:27:39 -0400

    complete logic to recursively search for longest word in string

--- fp/longest_word.py
+++ fp/longest_word.py
@@ -1,2 +1,13 @@
 def find_longest_word(document, longest_word=""):
-    pass
+    new_longest_word = longest_word
+    working_document = document.split(maxsplit=1)
+    if len(working_document) == 2:
+        if len(working_document[0]) > len(new_longest_word):
+            new_longest_word = working_document[0]
+        return find_longest_word(working_document[1], new_longest_word)
+    elif len(working_document) == 1:
+        if len(working_document[0]) > len(new_longest_word):
+            new_longest_word = working_document[0]
+        return new_longest_word
+    else:
+        return ""
