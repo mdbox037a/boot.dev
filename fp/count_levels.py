9aa878cd74da8d31f171c67bd6f03b99f3c1061d
matt box <mdbox037a@protonmail.com>
2025-10-10 21:38:07 -0400

    complete nested levels counter logic

--- fp/count_levels.py
+++ fp/count_levels.py
@@ -1,2 +1,13 @@
 def count_nested_levels(nested_documents, target_document_id, level=1):
-    pass
+    local_level = level
+    for document_id in nested_documents:
+        if document_id == target_document_id:
+            return local_level
+        else:
+            r_level = count_nested_levels(
+                nested_documents[document_id], target_document_id, (local_level + 1)
+            )
+            if r_level != -1:
+                return r_level
+
+    return -1
