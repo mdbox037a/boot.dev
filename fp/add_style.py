2853f6ebd263e10853d5cfe723756f9314e06d30
matt box <mdbox037a@protonmail.com>
2025-10-15 22:24:16 -0400

    complete css styles nested dictionary function

--- fp/add_style.py
+++ fp/add_style.py
@@ -1,2 +1,13 @@
+import copy
+
+
 def css_styles(initial_styles):
-    pass
+    copy_styles = copy.deepcopy(initial_styles)
+
+    def add_style(selector, property, value):
+        if selector not in copy_styles:
+            copy_styles[selector] = {}
+        copy_styles[selector][property] = value
+        return copy_styles
+
+    return add_style
