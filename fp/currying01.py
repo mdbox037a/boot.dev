abcc400808f53d434e4e1dcf09e9a58dbe7407c8
matt box <mdbox037a@protonmail.com>
2025-10-16 20:27:41 -0400

    create first short example of currying

---
+++ fp/currying01.py
@@ -0,0 +1,11 @@
+def converted_font_size(font_size):
+    def converted_doc_type(doc_type):
+        if doc_type == "txt":
+            return font_size
+        if doc_type == "md":
+            return font_size * 2
+        if doc_type == "docx":
+            return font_size * 3
+        raise ValueError("invalid doc type")
+
+    return converted_doc_type
