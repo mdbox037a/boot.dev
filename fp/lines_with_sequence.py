439bc44732e0581ce3c879ca183fe1b26688ac71
matt box <mdbox037a@protonmail.com>
2025-10-17 18:39:57 -0400

    complete second currying example

--- fp/lines_with_sequence.py
+++ fp/lines_with_sequence.py
@@ -2,6 +2,14 @@ def lines_with_sequence(char):
     def with_char(length):
         sequence = char * length
 
-        # ?
+        def with_length(doc):
+            count = 0
+            lines = doc.split()
+            for line in lines:
+                if sequence in line:
+                    count += 1
+            return count
+
+        return with_length
 
     return with_char
