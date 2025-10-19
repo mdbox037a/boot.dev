d54ad22379ea8cab02af5ea6f0aaeee3a410ebc8
matt box <mdbox037a@protonmail.com>
2025-10-18 21:07:49 -0400

    complete markdown image text formatter function with currying

--- fp/markdown_image.py
+++ fp/markdown_image.py
@@ -1,2 +1,17 @@
 def create_markdown_image(alt_text):
-    pass
+    enclosed_text = f"![{alt_text}]"
+
+    def handle_url(url):
+        escaped_url = url.replace("(", "%28").replace(")", "%29")
+        formatted_text = f"{enclosed_text}({escaped_url})"
+
+        def add_title(title=None):
+            if title is not None:
+                enclosed_title = f'"{title}"'
+                with_title = f"{formatted_text.replace(')', '')} {enclosed_title})"
+                return with_title
+            return formatted_text
+
+        return add_title
+
+    return handle_url
