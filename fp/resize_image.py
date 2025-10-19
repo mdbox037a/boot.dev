fa6ae6649710d374c848cd25b0fb276527cd5a9b
matt box <mdbox037a@protonmail.com>
2025-10-19 19:23:46 -0400

    complete image resizer with error checks

--- fp/resize_image.py
+++ fp/resize_image.py
@@ -1,2 +1,13 @@
 def new_resizer(max_width, max_height):
-    pass
+    def set_min(min_width=0, min_height=0):
+        if min_width > max_width or min_height > max_height:
+            raise Exception("minimum size cannot exceed maximum size")
+
+        def resize(width, height):
+            new_width = max(min(width, max_width), min_width)
+            new_height = max(min(height, max_height), min_height)
+            return new_width, new_height
+
+        return resize
+
+    return set_min
