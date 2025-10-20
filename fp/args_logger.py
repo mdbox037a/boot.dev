eb594f1225ac4604cd8cdd29f28ed01125cbbdab
matt box <mdbox037a@protonmail.com>
2025-10-20 16:57:53 -0400

    complete first try of args parsing function

--- fp/args_logger.py
+++ fp/args_logger.py
@@ -1,3 +1,11 @@
 def args_logger(*args, **kwargs):
-    # ?
+    count = 0
+    for arg in args:
+        count += 1
+        print(f"{count}. {arg}")
 
+    alpha_kwargs = sorted(kwargs.items())
+    count = 0
+    for entry in alpha_kwargs:
+        print(f"* {alpha_kwargs[count][0]}: {alpha_kwargs[count][1]}")
+        count += 1
