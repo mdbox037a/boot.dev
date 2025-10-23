9e4583dacf008af539adbfd425554f58ced94a2e
matt box <mdbox037a@protonmail.com>
2025-10-22 22:55:21 -0400

    complete palindrome detector logic try 1

--- fp/palindromes.py
+++ fp/palindromes.py
@@ -1,5 +1,15 @@
-# ?
+from functools import lru_cache
 
-# ?
+
+@lru_cache()
 def is_palindrome(word):
-    pass
+    char_list = list(word)
+    if len(char_list) == 0:
+        return True
+    elif (len(char_list) <= 2) and (char_list[0] == char_list[-1]):
+        return True
+    else:
+        if char_list[0] == char_list[-1]:
+            return is_palindrome("".join(char_list[1:-1]))
+        else:
+            return False
