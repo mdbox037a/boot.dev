23a58a8313dd47f65bb0a8443a5a16a034c56663
matt box <mdbox037a@protonmail.com>
2025-11-07 21:43:13 -0500

    create challenge file and test script; errors -> resolve

---
+++ challenges/fix_steps_to_zero/steps_to_zero.py
@@ -0,0 +1,15 @@
+"""
+return how many steps it takes to reduce a non-negative integer to 0 using these rules:
+- If the number is even, divide it by 2.
+- If the number is odd, subtract 1.
+Use a recursive solution. The function should be a pure function (no printing or modifying outside state).
+"""
+
+
+def steps_to_zero(n):
+    if n == 0:
+        return 0
+    if n % 2 == 0:
+        return steps_to_zero(n / 2) + 1
+    else:
+        return steps_to_zero(n - 1)
---
+++ challenges/fix_steps_to_zero/steps_to_zero_test.py
@@ -0,0 +1,53 @@
+from fix_steps_to_zero import *
+
+run_cases = [
+    (14, 6),
+    (8, 4),
+    (0, 0),
+]
+
+submit_cases = run_cases + [
+    (1, 1),
+    (123, 12),
+    (1000, 15),
+]
+
+
+def test(input1, expected_output):
+    print("---------------------------------")
+    print(f"Input number: {input1}")
+    result = steps_to_zero(input1)
+    print(f"Expected: {expected_output}")
+    print(f"Actual:   {result}")
+    if result == expected_output:
+        return True
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    skipped = len(submit_cases) - len(test_cases)
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+            print("Pass")
+        else:
+            failed += 1
+            print("Fail")
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    if skipped > 0:
+        print(f"{passed} passed, {failed} failed, {skipped} skipped")
+    else:
+        print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
