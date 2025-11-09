20229831af64d4b6d4d2b6554077dfb25a8f4069
matt box <mdbox037a@protonmail.com>
2025-11-08 20:24:13 -0500

    complete get_follower_prediction logic

--- dsa/follower_prediction/get_follower_prediction.py
+++ dsa/follower_prediction/get_follower_prediction.py
@@ -1,2 +1,10 @@
 def get_follower_prediction(follower_count, influencer_type, num_months):
-    pass
+    rate = 4
+    if influencer_type == "fitness":
+        rate = 4
+    elif influencer_type == "cosmetic":
+        rate = 3
+    else:
+        rate = 2
+
+    return follower_count * (rate**num_months)
