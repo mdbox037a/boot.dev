72173bed01be600281d3ca2e2305d855d71a7150
matt box <mdbox037a@protonmail.com>
2025-11-09 16:22:19 -0500

    complete logic to implement factorial calculation to determine possible orderings, given a number of posts

--- dsa/possible_orders/num_possible_orders.py
+++ dsa/possible_orders/num_possible_orders.py
@@ -1,2 +1,9 @@
 def num_possible_orders(num_posts):
-    pass
+    total = 1
+    for i in range(1, num_posts + 1):
+        total *= i
+    return total
+
+
+# don't need to make a special case for num_posts == 0, because total is set
+# to 1 from the beginning and the for loop just won't run if num_posts == 0
