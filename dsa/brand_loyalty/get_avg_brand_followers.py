486603ed875a2265905d2b9b174deb850805465f
matt box <mdbox037a@protonmail.com>
2025-11-12 17:18:53 -0500

    complete O(nm) example

--- dsa/brand_loyalty/get_avg_brand_followers.py
+++ dsa/brand_loyalty/get_avg_brand_followers.py
@@ -1,2 +1,10 @@
 def get_avg_brand_followers(all_handles, brand_name):
-    pass
+    matches = 0
+    num_lists = len(all_handles)
+    for influencer_list in all_handles:
+        for handle in influencer_list:
+            if brand_name in handle:
+                matches += 1
+    if num_lists == 0:
+        return None
+    return matches / num_lists
