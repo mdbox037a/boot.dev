8a983d96f4d7f2f4d9c5f315095e63f93dd684e1
matt box <mdbox037a@protonmail.com>
2025-11-16 10:55:05 -0500

    complete merge sort logic

--- dsa/sorting_algorithms/merge_sort/main.py
+++ dsa/sorting_algorithms/merge_sort/main.py
@@ -1,6 +1,31 @@
 def merge_sort(nums):
-    pass
+    if len(nums) < 2:
+        return nums
+    half = len(nums) // 2
+    nums1 = nums[:half]
+    nums2 = nums[half:]
+    sorted1 = merge_sort(nums1)
+    sorted2 = merge_sort(nums2)
+    return merge(sorted1, sorted2)
 
 
 def merge(first, second):
-    pass
+    final = []
+    i = 0
+    j = 0
+    while (i < len(first)) and (j < len(second)):
+        if first[i] <= second[j]:
+            final.append(first[i])
+            i += 1
+        else:
+            final.append(second[j])
+            j += 1
+    if i < len(first):
+        while i < len(first):
+            final.append(first[i])
+            i += 1
+    elif j < len(second):
+        while j < len(second):
+            final.append(second[j])
+            j += 1
+    return final
