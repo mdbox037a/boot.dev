e9772d621ca41acc1b48fc47a4ac99b8f92ec888
matt box <mdbox037a@protonmail.com>
2025-11-15 22:09:49 -0500

    create vanity score main and testing script

---
+++ dsa/sorting_algorithms/vanity/main.py
@@ -0,0 +1,18 @@
+class Influencer:
+    def __init__(self, num_selfies, num_bio_links):
+        self.num_selfies = num_selfies
+        self.num_bio_links = num_bio_links
+
+    def __repr__(self):
+        return f"({self.num_selfies}, {self.num_bio_links})"
+
+
+# dont touch above this line
+
+
+def vanity(influencer):
+    pass
+
+
+def vanity_sort(influencers):
+    pass
