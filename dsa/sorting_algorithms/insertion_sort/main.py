229e31dd9ad30270e717c182f89dcd04eb7fdf02
matt box <mdbox037a@protonmail.com>
2025-11-17 21:27:34 -0500

    complete draft of insertion sort algorithm

--- dsa/sorting_algorithms/insertion_sort/main.py
+++ dsa/sorting_algorithms/insertion_sort/main.py
@@ -1,2 +1,7 @@
 def insertion_sort(nums):
-    pass
+    for i in range(1, len(nums)):
+        j = i
+        while j > 0 and nums[j - 1] > nums[j]:
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            j -= 1
+    return nums
