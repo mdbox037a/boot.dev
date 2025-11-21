93c542bf36f9b4752cf47468dfb20256a1be9aac
matt box <mdbox037a@protonmail.com>
2025-11-20 21:23:10 -0500

    refactor with for loop

--- dsa/exponential_time/main.py
+++ dsa/exponential_time/main.py
@@ -1,14 +1,12 @@
 def fib(n):
+    if n == 0:
+        return n
+
     grandparent = 0
     parent = 1
     current = 1
 
-    if n == 0:
-        return 0
-
-    i = n - 1
-    while i > 0:
+    for i in range(0, n - 1):
         current = parent + grandparent
         grandparent, parent = parent, current
-        i -= 1
     return current
