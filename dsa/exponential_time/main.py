fa4a831328c44e056495a459310fe6fca8a70c9e
matt box <mdbox037a@protonmail.com>
2025-11-20 21:09:15 -0500

    complete first draft of polynomial rewrite

--- dsa/exponential_time/main.py
+++ dsa/exponential_time/main.py
@@ -1,6 +1,14 @@
 def fib(n):
+    grandparent = 0
+    parent = 1
+    current = 1
+
     if n == 0:
         return 0
-    if n == 1:
-        return 1
-    return fib(n - 1) + fib(n - 2)
+
+    i = n - 1
+    while i > 0:
+        current = parent + grandparent
+        grandparent, parent = parent, current
+        i -= 1
+    return current
