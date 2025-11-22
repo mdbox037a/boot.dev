5dd5a1db7e8fc411413155119b0a482754f5f14b
matt box <mdbox037a@protonmail.com>
2025-11-21 22:34:54 -0500

    complete power_set algorithm first draft

--- dsa/exponential_time/power_set/main.py
+++ dsa/exponential_time/power_set/main.py
@@ -1,2 +1,14 @@
 def power_set(input):
-    pass
+    empty_list = []
+    if len(input) == 0:
+        result = [empty_list]
+        return result
+    else:
+        all_subsets = [empty_list]
+        for i in input:
+            new_subsets = []
+            for subset in all_subsets:
+                new_subset = subset + [i]
+                new_subsets.append(new_subset)
+            all_subsets.extend(new_subsets)
+        return all_subsets
