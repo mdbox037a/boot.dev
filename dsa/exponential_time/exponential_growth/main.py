c3aca26b42647a72254929e3f7139db14f95ed0e
matt box <mdbox037a@protonmail.com>
2025-11-22 16:29:33 -0500

    complete exponential growth logic

--- dsa/exponential_time/exponential_growth/main.py
+++ dsa/exponential_time/exponential_growth/main.py
@@ -1,2 +1,7 @@
 def exponential_growth(n, factor, days):
-    pass
+    growth_sequence = []
+    followers = n
+    for day in range(0, days + 1):
+        growth_sequence.append(followers)
+        followers = followers * factor
+    return growth_sequence
