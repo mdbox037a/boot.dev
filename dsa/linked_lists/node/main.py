20674fd1e9b9f895e9112115d1429675fa63e97c
matt box <mdbox037a@protonmail.com>
2025-11-26 10:51:34 -0500

    add skeleton for linked list first exercise - nodes - and test script

---
+++ dsa/linked_lists/node/main.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, val):
+        pass
+
+    def set_next(self, node):
+        pass
+
+    # don't touch below this line
+
+    def __repr__(self):
+        return self.val
