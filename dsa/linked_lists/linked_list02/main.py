5c569328306b93836726e2849f86a74cb2ede7dd
matt box <mdbox037a@protonmail.com>
2025-11-26 11:06:06 -0500

    create skeleton file to implement adding new tail nodes

---
+++ dsa/linked_lists/linked_list02/main.py
@@ -0,0 +1,23 @@
+from node import Node
+
+
+class LinkedList:
+    def add_to_tail(self, node):
+        pass
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " -> ".join(nodes)
