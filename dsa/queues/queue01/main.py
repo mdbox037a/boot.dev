70ea051a50f1666b147465c0619731472db6b5b4
matt box <mdbox037a@protonmail.com>
2025-11-26 10:33:47 -0500

    implement queue basic operations

--- dsa/queues/queue01/main.py
+++ dsa/queues/queue01/main.py
@@ -3,13 +3,19 @@ class Queue:
         self.items = []
 
     def push(self, item):
-        pass
+        self.items.insert(0, item)
 
     def pop(self):
-        pass
+        if len(self.items) == 0:
+            return None
+        holder = self.items[-1]
+        del self.items[-1]
+        return holder
 
     def peek(self):
-        pass
+        if len(self.items) == 0:
+            return None
+        return self.items[-1]
 
     def size(self):
-        pass
+        return len(self.items)
