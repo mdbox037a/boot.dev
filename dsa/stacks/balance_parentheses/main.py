6fa13d55f4555b6cb55dac3122837cb64336f02a
matt box <mdbox037a@protonmail.com>
2025-11-25 19:37:00 -0500

    complete first draft of balanced parentheses algorithmusing a stack data structure

--- dsa/stacks/balance_parentheses/main.py
+++ dsa/stacks/balance_parentheses/main.py
@@ -2,4 +2,15 @@ from stack import Stack
 
 
 def is_balanced(input_str):
-    pass
+    stack = Stack()
+    for char in input_str:
+        if char == "(":
+            stack.push(char)
+        elif char == ")" and stack.size() == 0:
+            stack.push(char)
+        elif char == ")":
+            stack.pop()
+    if stack.size() != 0:
+        return False
+    else:
+        return True
