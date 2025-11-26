56543882e604497fbbf9bb79a1c161d3673133f5
matt box <mdbox037a@protonmail.com>
2025-11-26 10:29:19 -0500

    create queue implementation file skeleton and test script

---
+++ dsa/queues/queue01/main.py
@@ -0,0 +1,15 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def size(self):
+        pass
