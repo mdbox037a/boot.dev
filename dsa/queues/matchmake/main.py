946990a22d697199c9551905d09d2649f1e7f2e6
matt box <mdbox037a@protonmail.com>
2025-11-26 10:47:57 -0500

    complete matchmake logic

--- dsa/queues/matchmake/main.py
+++ dsa/queues/matchmake/main.py
@@ -1,5 +1,19 @@
 from queue import Queue
 
+queue = Queue()
+
 
 def matchmake(queue, user):
-    pass
+    name = user[0]
+    action = user[1]
+    if action is "leave":
+        queue.search_and_remove(name)
+    elif action is "join":
+        queue.push(name)
+
+    if queue.size() >= 4:
+        user1 = queue.pop()
+        user2 = queue.pop()
+        return f"{user1} matched {user2}!"
+    else:
+        return "No match found"
