00eeff47df5417a6a2dea7e3e6a21273d77cd3f5
matt box <mdbox037a@protonmail.com>
2025-12-04 22:55:10 -0500

    begin work on first iteration of red/black trees; add testing script

---
+++ dsa/trees/red_black_trees/main.py
@@ -0,0 +1,19 @@
+class RBNode:
+    def __init__(self, val):
+        self.red = False
+        self.parent = None
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class RBTree:
+    def __init__(self):
+        self.nil = RBNode(None)
+        self.nil.red = False
+        self.nil.left = None
+        self.nil.right = None
+        self.root = self.nil
+
+    def insert(self, val):
+        pass
