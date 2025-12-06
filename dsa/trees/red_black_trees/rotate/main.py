c0f6686ce99403fc280e6aed20ae8dee9648ed20
matt box <mdbox037a@protonmail.com>
2025-12-06 17:09:28 -0500

    create skeleton file to implement rotate_left/right methods; add testing script and user file

---
+++ dsa/trees/red_black_trees/rotate/main.py
@@ -0,0 +1,51 @@
+class RBNode:
+    def __init__(self, val):
+        self.red = False
+        self.parent = None
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class RBTree:
+    def __init__(self):
+        self.nil = RBNode(None)
+        self.nil.red = False
+        self.nil.left = None
+        self.nil.right = None
+        self.root = self.nil
+
+    def rotate_left(self, pivot_parent):
+        pass
+
+    def rotate_right(self, pivot_parent):
+        pass
+
+        # don't touch below this line
+
+    def insert(self, val):
+        new_node = RBNode(val)
+        new_node.parent = None
+        new_node.left = self.nil
+        new_node.right = self.nil
+        new_node.red = True
+
+        parent = None
+        current = self.root
+        while current != self.nil:
+            parent = current
+            if new_node.val < current.val:
+                current = current.left
+            elif new_node.val > current.val:
+                current = current.right
+            else:
+                # duplicate, just ignore
+                return
+
+        new_node.parent = parent
+        if parent is None:
+            self.root = new_node
+        elif new_node.val < parent.val:
+            parent.left = new_node
+        else:
+            parent.right = new_node
