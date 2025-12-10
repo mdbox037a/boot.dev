5818881cc29fc005948c346b70cc7ec239f8e4f4
matt box <mdbox037a@protonmail.com>
2025-12-09 20:21:22 -0500

    complete quick hashing function demo

--- dsa/hashmaps/hash_function/main.py
+++ dsa/hashmaps/hash_function/main.py
@@ -1,6 +1,9 @@
 class HashMap:
     def key_to_index(self, key):
-        pass
+        uSum = 0
+        for char in key:
+            uSum += ord(char)
+        return int(uSum % len(self.hashmap))
 
     # don't touch below this line
 
