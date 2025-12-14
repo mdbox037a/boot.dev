61b39f60d8209d673cd601e6ab55685f4155c73d
matt box <mdbox037a@protonmail.com>
2025-12-14 15:31:39 -0500

    complete add() method for trie structure

--- dsa/tries/add/main.py
+++ dsa/tries/add/main.py
@@ -1,6 +1,11 @@
 class Trie:
     def add(self, word):
-        pass
+        current_level = self.root
+        for char in word:
+            if char not in current_level:
+                current_level[char] = {}
+            current_level = current_level[char]
+        current_level[self.end_symbol] = True
 
     # don't touch below this line
 
