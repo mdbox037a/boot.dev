ecd944b05baa7090de02edf364d8bc59d1e44e13
matt box <mdbox037a@protonmail.com>
2025-12-15 20:38:40 -0500

    create exists() method space and add test script

---
+++ dsa/tries/exists/main.py
@@ -0,0 +1,17 @@
+class Trie:
+    def exists(self, word):
+        pass
+
+    # don't touch below this line
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
