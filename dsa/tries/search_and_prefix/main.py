5cc4c884e4933c128a3fc7f47d121f6d27202442
matt box <mdbox037a@protonmail.com>
2025-12-16 20:11:13 -0500

    create search_level() and words_with_prefix() method skeleton file and add test script

---
+++ dsa/tries/search_and_prefix/main.py
@@ -0,0 +1,20 @@
+class Trie:
+    def search_level(self, current_level, current_prefix, words):
+        pass
+
+    def words_with_prefix(self, prefix):
+        pass
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+    def add(self, word):
+        current_level = self.root
+        for letter in word:
+            if letter not in current_level:
+                current_level[letter] = {}
+            current_level = current_level[letter]
+        current_level[self.end_symbol] = True
