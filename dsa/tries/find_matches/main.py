0e649019c372178a5cdae2a7e960c6b3072d238e
matt box <mdbox037a@protonmail.com>
2025-12-17 20:08:40 -0500

    create skeleton file to implement find_matches() method and add test script

---
+++ dsa/tries/find_matches/main.py
@@ -0,0 +1,17 @@
+class Trie:
+    def find_matches(self, document):
+        pass
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
