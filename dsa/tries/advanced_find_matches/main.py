e6653e7135942ffb5731a1f4db87ce1fb09afc7c
matt box <mdbox037a@protonmail.com>
2025-12-19 18:00:11 -0500

    create skeleton file to begin work on advanced matches finder function and add test script

---
+++ dsa/tries/advanced_find_matches/main.py
@@ -0,0 +1,30 @@
+class Trie:
+    def advanced_find_matches(self, document, variations):
+        pass
+
+    # don't touch below this line
+
+    def find_matches(self, document):
+        matches = set()
+        for i in range(len(document)):
+            level = self.root
+            for j in range(i, len(document)):
+                ch = document[j]
+                if ch not in level:
+                    break
+                level = level[ch]
+                if self.end_symbol in level:
+                    matches.add(document[i : j + 1])
+        return matches
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
