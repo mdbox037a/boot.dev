6f16774189481984f40d5cfc9d21540659e0c420
matt box <mdbox037a@protonmail.com>
2025-12-18 20:32:24 -0500

    create longest common prefix skeleton file and add testing script

---
+++ dsa/tries/longest_common_prefix/main.py
@@ -0,0 +1,17 @@
+class Trie:
+    def longest_common_prefix(self):
+        pass
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
