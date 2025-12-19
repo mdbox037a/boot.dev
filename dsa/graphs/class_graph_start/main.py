026c1750a68ec30e3736b8b323faa10823d9409d
matt box <mdbox037a@protonmail.com>
2025-12-19 18:18:27 -0500

    begin work on graph data strcutures with __init__ and add_edge methods implementations to start, plus test script

---
+++ dsa/graphs/class_graph_start/main.py
@@ -0,0 +1,18 @@
+class Graph:
+    def __init__(self, num_vertices):
+        pass
+
+    def add_edge(self, u, v):
+        pass
+
+    # don't touch below this line
+
+    def edge_exists(self, u, v):
+        if u < 0 or u >= len(self.graph):
+            return False
+        if len(self.graph) == 0:
+            return False
+        row1 = self.graph[0]
+        if v < 0 or v >= len(row1):
+            return False
+        return self.graph[u][v]
