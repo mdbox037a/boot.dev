a038c93caadca79860950549e87e5ec80dfd50ed
matt box <mdbox037a@protonmail.com>
2025-12-23 20:20:11 -0500

    complete first draft of breadth first search with recursion

--- dsa/graphs/dfs/main.py
+++ dsa/graphs/dfs/main.py
@@ -1,9 +1,15 @@
 class Graph:
     def depth_first_search(self, start_vertex):
-        pass
+        visited = []
+        self.depth_first_search_r(visited, start_vertex)
+        return visited
 
     def depth_first_search_r(self, visited, current_vertex):
-        pass
+        visited.append(current_vertex)
+        neighbors = sorted(self.graph[current_vertex])
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                self.depth_first_search_r(visited, neighbor)
 
         # don't touch below this line
 
