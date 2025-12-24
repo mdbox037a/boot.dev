74cdf506cd8a6a830ff6748b943572784f524a2f
matt box <mdbox037a@protonmail.com>
2025-12-24 15:44:08 -0500

    begin work on traveling salesman solutions with skeleton file and testing script

---
+++ dsa/p_vs_np/traveling_salesman/main.py
@@ -0,0 +1,25 @@
+def tsp(cities, paths, dist):
+    pass
+
+
+# don't touch below this line
+
+
+def permutations(arr):
+    res = []
+    res = helper(res, arr, len(arr))
+    return res
+
+
+def helper(res, arr, n):
+    if n == 1:
+        tmp = arr.copy()
+        res.append(tmp)
+    else:
+        for i in range(n):
+            res = helper(res, arr, n - 1)
+            if n % 2 == 1:
+                arr[n - 1], arr[i] = arr[i], arr[n - 1]
+            else:
+                arr[0], arr[n - 1] = arr[n - 1], arr[0]
+    return res
