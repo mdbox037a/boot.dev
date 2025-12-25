c73c1ba450622a3ebb5c99e17a7c470f261ebd67
matt box <mdbox037a@protonmail.com>
2025-12-25 15:58:11 -0500

    complete draft 1 of tsp verifier algorithm

--- dsa/p_vs_np/tsp_verifier/main.py
+++ dsa/p_vs_np/tsp_verifier/main.py
@@ -1,2 +1,8 @@
 def verify_tsp(paths, dist, actual_path):
-    pass
+    total = 0
+    for city in range(len(actual_path) - 1):
+        total += paths[actual_path[city]][actual_path[city + 1]]
+    if total < dist:
+        return True
+    else:
+        return False
