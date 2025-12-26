163c2a42bd9e67f1c827a87d59bbf5143198f885
matt box <mdbox037a@protonmail.com>
2025-12-26 16:10:40 -0500

    complete quick password brute force calculator function

--- dsa/p_vs_np/passwords/main.py
+++ dsa/p_vs_np/passwords/main.py
@@ -1,2 +1,8 @@
 def get_num_guesses(length):
-    pass
+    char_set_size = 26
+    counter = 1
+    num_guesses = 0
+    while counter < (length + 1):
+        num_guesses += char_set_size**counter
+        counter += 1
+    return num_guesses
