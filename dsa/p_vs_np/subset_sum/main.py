71d501dbd0b491a99665255d8e0997903c016901
matt box <mdbox037a@protonmail.com>
2025-12-27 16:24:22 -0500

    complete subset sum brute force algorithm

--- dsa/p_vs_np/subset_sum/main.py
+++ dsa/p_vs_np/subset_sum/main.py
@@ -1,6 +1,19 @@
 def subset_sum(nums, target):
-    pass
+    return find_subset_sum(nums, target, len(nums) - 1)
 
 
 def find_subset_sum(nums, target, index):
-    pass
+    if target == 0:
+        return True
+    elif index < 0:
+        return False
+
+    if nums[index] > target:
+        return find_subset_sum(nums, target, index - 1)
+    else:
+        resultA = find_subset_sum(nums, target, index - 1)
+        resultB = find_subset_sum(nums, target - nums[index], index - 1)
+        if resultA or resultB:
+            return True
+        else:
+            return False
