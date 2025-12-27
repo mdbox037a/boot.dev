03ec8e1c95484929e83e8662c43a56e40289b242
matt box <mdbox037a@protonmail.com>
2025-12-27 16:03:54 -0500

    complete simple prime factorization algorithm

--- dsa/p_vs_np/prime_factorization/main.py
+++ dsa/p_vs_np/prime_factorization/main.py
@@ -2,4 +2,16 @@ import math
 
 
 def prime_factors(n):
-    pass
+    factors_list = []
+    while (n % 2) == 0:
+        n //= 2
+        factors_list.append(2)
+
+    limit = int(math.sqrt(n)) + 1
+    for i in range(3, limit, 2):
+        while (n % i) == 0:
+            n //= i
+            factors_list.append(i)
+    if n > 2:
+        factors_list.append(n)
+    return sorted(factors_list)
