075e2ed8bd75815e93c05be87e998b41c04bbbed
matt box <mdbox037a@protonmail.com>
2026-02-04 21:18:47 -0500

    complete compound logical operators exercise

---
+++ mm_in_c/c_basics/logical_operators.c
@@ -0,0 +1,8 @@
+#include "exercise.h"
+
+int can_access_registry(int is_premium, int reputation, int has_2fa) {
+	if (is_premium == 1 || (reputation >= 100 && has_2fa == 1))
+		return 1;
+	else
+		return 0;
+}
