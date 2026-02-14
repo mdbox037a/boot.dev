61dea3bee247240d77330e1cc2bce950944de135
matt box <mdbox037a@protonmail.com>
2026-02-13 21:10:53 -0500

    complete strcpy clone as exercise

---
+++ mm_in_c/pointers/concat_strings.c
@@ -0,0 +1,12 @@
+#include "exercise.h"
+#include <stdio.h>
+
+void concat_strings(char *str1, const char *str2) {
+	int i, j;
+
+	for (i = 0; *(str1 + i) != '\0'; i++)
+		;
+	for (j = 0; *(str2 + j) != '\0'; j++, i++)
+		*(str1 + i) = *(str2 + j);
+	*(str1 + i) = '\0';
+}
