c64429e9a1825b5107b2ba1ee1519b76d39712f0
matt box <mdbox037a@protonmail.com>
2026-02-22 15:33:03 -0500

    complete first exercise in allocating heap space for longer-lived value storage

---
+++ mm_in_c/stack_heap/the_heap.c
@@ -0,0 +1,9 @@
+#include "exercise.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_full_greeting(char *greeting, char *name, int size) {
+	char *full_greeting = malloc(size * sizeof(char));
+	snprintf(full_greeting, size * sizeof(char), "%s %s", greeting, name);
+	return full_greeting;
+}
