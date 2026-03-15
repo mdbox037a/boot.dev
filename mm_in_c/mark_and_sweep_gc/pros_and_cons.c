39d00a6033a42e0e001cfb1765733029ba9a4707
Matt Box <mdbox037a@protonmail.com>
2026-03-15 14:19:52 -0400

    implement vm stack initializer and free functions

---
+++ mm_in_c/mark_and_sweep_gc/pros_and_cons.c
@@ -0,0 +1,17 @@
+#include "vm.h"
+
+vm_t *vm_new() {
+        vm_t *vm = malloc(sizeof(vm_t));
+        if (vm == NULL) {
+                return NULL;
+        }
+        vm->frames = stack_new(8);
+        vm->objects = stack_new(8);
+        return vm;
+}
+
+void vm_free(vm_t *vm) {
+        stack_free(vm->frames);
+        stack_free(vm->objects);
+        free(vm);
+}
