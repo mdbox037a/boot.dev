3660f1aca19eca0d048c5a8d1596cd4e6e440cf7
Matt Box <mdbox037a@protonmail.com>
2026-03-15 14:26:58 -0400

    create outline to implement wrappers to prevent non-stack data being pushed onto stacks

---
+++ mm_in_c/mark_and_sweep_gc/stack_frames.c
@@ -0,0 +1,33 @@
+#include "vm.h"
+
+void vm_frame_push(vm_t *vm, frame_t *frame) {
+        // ?
+}
+
+frame_t *vm_new_frame(vm_t *vm) {
+        // ?
+}
+
+void frame_free(frame_t *frame) {
+        // ?
+}
+
+vm_t *vm_new() {
+        vm_t *vm = malloc(sizeof(vm_t));
+        if (vm == NULL) {
+                return NULL;
+        }
+
+        vm->frames = stack_new(8);
+        vm->objects = stack_new(8);
+        return vm;
+}
+
+void vm_free(vm_t *vm) {
+        for (int i = 0; i < vm->frames->count; i++) {
+                frame_free(vm->frames->data[i]);
+        }
+        stack_free(vm->frames);
+        stack_free(vm->objects);
+        free(vm);
+}
