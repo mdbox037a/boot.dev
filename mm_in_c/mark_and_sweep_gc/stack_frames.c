83cf80596baa6a4534fbc6fe611686528a59344b
Matt Box <mdbox037a@protonmail.com>
2026-03-15 14:49:28 -0400

    implement frames functions

--- mm_in_c/mark_and_sweep_gc/stack_frames.c
+++ mm_in_c/mark_and_sweep_gc/stack_frames.c
@@ -1,15 +1,17 @@
 #include "vm.h"
 
-void vm_frame_push(vm_t *vm, frame_t *frame) {
-        // ?
-}
+void vm_frame_push(vm_t *vm, frame_t *frame) { stack_push(vm->frames, frame); }
 
 frame_t *vm_new_frame(vm_t *vm) {
-        // ?
+        frame_t *frame = malloc(sizeof(frame_t));
+        frame->references = stack_new(8);
+        vm_frame_push(vm, frame);
+        return frame;
 }
 
 void frame_free(frame_t *frame) {
-        // ?
+        stack_free(frame->references);
+        free(frame);
 }
 
 vm_t *vm_new() {
