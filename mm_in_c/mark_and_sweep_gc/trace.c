819357760fc4720edbd30a533aa2a8bc904c31b4
Matt Box <mdbox037a@protonmail.com>
2026-03-17 22:03:23 -0400

    create starter file to begin trace implementation

---
+++ mm_in_c/mark_and_sweep_gc/trace.c
@@ -0,0 +1,76 @@
+#include "stack.h"
+#include "vm.h"
+
+void trace(vm_t *vm) {
+        // ?
+}
+
+void trace_blacken_object(stack_t *gray_objects, snek_object_t *obj) {
+        // ?
+}
+
+void trace_mark_object(stack_t *gray_objects, snek_object_t *obj) {
+        // ?
+}
+
+// don't touch below this line
+
+void mark(vm_t *vm) {
+        for (size_t i = 0; i < vm->frames->count; i++) {
+                frame_t *frame = vm->frames->data[i];
+                for (size_t j = 0; j < frame->references->count; j++) {
+                        snek_object_t *obj = frame->references->data[j];
+                        obj->is_marked = true;
+                }
+        }
+}
+
+void frame_reference_object(frame_t *frame, snek_object_t *obj) {
+        stack_push(frame->references, obj);
+}
+
+vm_t *vm_new() {
+        vm_t *vm = malloc(sizeof(vm_t));
+        if (vm == NULL) {
+                return NULL;
+        }
+
+        vm->frames = stack_new(8);
+        vm->objects = stack_new(8);
+        return vm;
+}
+
+void vm_free(vm_t *vm) {
+        // Free the stack frames, and then their container
+        for (int i = 0; i < vm->frames->count; i++) {
+                frame_free(vm->frames->data[i]);
+        }
+        stack_free(vm->frames);
+
+        // Free the objects, and then their container
+        for (int i = 0; i < vm->objects->count; i++) {
+                snek_object_free(vm->objects->data[i]);
+        }
+        stack_free(vm->objects);
+
+        free(vm);
+}
+
+void vm_frame_push(vm_t *vm, frame_t *frame) { stack_push(vm->frames, frame); }
+
+frame_t *vm_new_frame(vm_t *vm) {
+        frame_t *frame = malloc(sizeof(frame_t));
+        frame->references = stack_new(8);
+
+        vm_frame_push(vm, frame);
+        return frame;
+}
+
+void frame_free(frame_t *frame) {
+        stack_free(frame->references);
+        free(frame);
+}
+
+void vm_track_object(vm_t *vm, snek_object_t *obj) {
+        stack_push(vm->objects, obj);
+}
