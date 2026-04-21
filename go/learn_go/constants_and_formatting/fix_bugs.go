175590891fb5d0755d70ee3fef3a5923e746bf8f
Matt Box <mdbox037a@protonmail.com>
2026-04-20 20:43:50 -0400

    complete small bugfixes

---
+++ go/learn_go/constants_and_formatting/fix_bugs.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var startup string = "Textio SMS service booting up..."
+	var message string = "Sending test message"
+	var confirm string = "Message sent!"
+
+	fmt.Println(startup)
+	fmt.Println(message)
+	fmt.Println(confirm)
+}
