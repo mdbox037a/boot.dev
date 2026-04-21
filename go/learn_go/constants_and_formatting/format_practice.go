b48c99939492186b40d7710faadf4702e7194d5e
Matt Box <mdbox037a@protonmail.com>
2026-04-20 20:54:34 -0400

    complete sprintf log message setup

---
+++ go/learn_go/constants_and_formatting/format_practice.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fname := "Dalinar"
+	lname := "Kholin"
+	age := 45
+	messageRate := 0.5
+	isSubscribed := false
+	message := "Sometimes a hypocrite is nothing more than a man in the process of changing."
+
+	userLog := fmt.Sprintf("Name: %s %s, Age: %d, Rate: %.1f, Is Subscribed: %t, Message: %s",
+		fname, lname, age, messageRate, isSubscribed, message)
+
+	fmt.Println(userLog)
+}
