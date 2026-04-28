a58bece74e16638354ca41cc9493204ea7cb05eb
Matt Box <mdbox037a@protonmail.com>
2026-04-27 20:37:50 -0400

    complete first example with type assertions on interface elements

---
+++ go/learn_go/interfaces/type_assertions.go
@@ -0,0 +1,49 @@
+package main
+
+func getExpenseReport(e expense) (string, float64) {
+	x, ok := e.(email)
+	if ok {
+		return x.toAddress, x.cost()
+	}
+	y, ok := e.(sms)
+	if ok {
+		return y.toPhoneNumber, y.cost()
+	}
+	return "", 0.0
+}
+
+type expense interface {
+	cost() float64
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
+}
