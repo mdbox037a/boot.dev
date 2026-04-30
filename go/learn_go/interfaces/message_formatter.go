4409f96c46f93dc7657ed071bc99c78865691a32
Matt Box <mdbox037a@protonmail.com>
2026-04-29 22:55:22 -0400

    complete interface-satisfying structs with struct methods

--- go/learn_go/interfaces/message_formatter.go
+++ go/learn_go/interfaces/message_formatter.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
+type formatter interface {
+	format() string
+}
+
+type plainText struct {
+	message string
+}
+
+func (p plainText) format() string {
+	return p.message
+}
+
+type bold struct {
+	message string
+}
+
+func (b bold) format() string {
+	return fmt.Sprintf("**%s**", b.message)
+}
+
+type code struct {
+	message string
+}
+
+func (c code) format() string {
+	return fmt.Sprintf("`%s`", c.message)
+}
+
 func sendMessage(format formatter) string {
 	return format.format() // Adjusted to call Format without an argument
 }
