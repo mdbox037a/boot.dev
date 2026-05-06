8b005bf7f9da0e19cfaff501af5ec41774fae841
Matt Box <mdbox037a@protonmail.com>
2026-05-05 20:42:45 -0400

    provide one solution for fizzbuzz

--- go/learn_go/loops/fizzbuzz.go
+++ go/learn_go/loops/fizzbuzz.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func fizzbuzz() {
-	// ?
+	threer := 0
+	fiver := 0
+	for n := 1; n < 101; n++ {
+		threer = n % 3
+		fiver = n % 5
+
+		if threer == 0 && fiver == 0 {
+			fmt.Println("fizzbuzz")
+		} else if threer == 0 {
+			fmt.Println("fizz")
+		} else if fiver == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(n)
+		}
+	}
 }
 
 func main() {
