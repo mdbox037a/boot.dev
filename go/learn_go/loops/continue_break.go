2bf3f775baae0ecba2cdd19ef3d11d0e29592bad
Matt Box <mdbox037a@protonmail.com>
2026-05-06 20:46:20 -0400

    complete prime number printing function

--- go/learn_go/loops/continue_break.go
+++ go/learn_go/loops/continue_break.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func printPrimes(max int) {
-	// ?
+	if max >= 2 {
+		isPrime := true
+		fmt.Println(2)
+		for n := 3; n <= max; n += 2 {
+			isPrime = true
+			for i := 3; i <= int(math.Sqrt(float64(n))); i += 2 {
+				if n%i == 0 {
+					isPrime = false
+					break
+				}
+			}
+			if isPrime {
+				fmt.Println(n)
+			}
+		}
+	}
 }
 
 func test(max int) {
