9f804ae67489e84111becd12edded5dad39e8d36
Matt Box <mdbox037a@protonmail.com>
2026-05-07 13:46:17 -0400

    complete first example with arrays

---
+++ go/learn_go/slices/arrays.go
@@ -0,0 +1,7 @@
+package main
+
+func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]int) {
+	messages := [3]string{primary, secondary, tertiary}
+	costs := [3]int{len(primary), len(primary) + len(secondary), len(primary) + len(secondary) + len(tertiary)}
+	return messages, costs
+}
