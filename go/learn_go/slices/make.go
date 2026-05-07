d6350b013aa88f1bfb83a511d56043bae6046a2c
Matt Box <mdbox037a@protonmail.com>
2026-05-07 14:15:00 -0400

    complete first exercise with make and slice

--- go/learn_go/slices/make.go
+++ go/learn_go/slices/make.go
@@ -1,5 +1,9 @@
 package main
 
 func getMessageCosts(messages []string) []float64 {
-	// ?
+	costs := make([]float64, len(messages))
+	for i := 0; i < len(costs); i++ {
+		costs[i] = float64(len(messages[i])) * 0.01
+	}
+	return costs
 }
