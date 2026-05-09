7d46b2ae49284671c49bcadf6698e6204622a5d7
Matt Box <mdbox037a@protonmail.com>
2026-05-08 21:23:44 -0400

    complete matrix-generating function for multiplication tables

--- go/learn_go/slices/slice_of_slices.go
+++ go/learn_go/slices/slice_of_slices.go
@@ -1,5 +1,13 @@
 package main
 
 func createMatrix(rows, cols int) [][]int {
-	return nil
+	chart := [][]int{}
+	for i := range rows {
+		holderRow := []int{}
+		for j := range cols {
+			holderRow = append(holderRow, i*j)
+		}
+		chart = append(chart, holderRow)
+	}
+	return chart
 }
