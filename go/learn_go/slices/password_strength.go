6027dc4d76103c5e0f4b818fcfbaf4030f280eb4
Matt Box <mdbox037a@protonmail.com>
2026-05-09 23:16:50 -0400

    complete password strength checking function

--- go/learn_go/slices/password_strength.go
+++ go/learn_go/slices/password_strength.go
@@ -1,5 +1,21 @@
 package main
 
 func isValidPassword(password string) bool {
-	// ?
+	if len(password) < 5 || len(password) > 12 {
+		return false
+	}
+	hasUpperCase := false
+	hasDigit := false
+	for _, char := range password {
+		if char >= 'A' && char <= 'Z' {
+			hasUpperCase = true
+		}
+		if char >= '0' && char <= '9' {
+			hasDigit = true
+		}
+		if hasUpperCase && hasDigit {
+			return true
+		}
+	}
+	return false
 }
