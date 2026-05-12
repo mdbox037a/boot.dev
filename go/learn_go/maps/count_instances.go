8ce847f16d4836b883dcbd5c097e52e3b6ae2824
Matt Box <mdbox037a@protonmail.com>
2026-05-11 20:49:13 -0400

    rewrite conditional logic to protect rest of function from temp ok

--- go/learn_go/maps/count_instances.go
+++ go/learn_go/maps/count_instances.go
@@ -2,9 +2,8 @@ package main
 
 func updateCounts(messagedUsers []string, validUsers map[string]int) {
 	for _, user := range messagedUsers {
-		_, ok := validUsers[user]
-		if ok {
-			validUsers[user] += 1
+		if _, ok := validUsers[user]; ok {
+			validUsers[user]++
 		}
 	}
 }
