b4bc1746316e4a494b8d027c73285ae5e039c829
Matt Box <mdbox037a@protonmail.com>
2026-05-11 20:19:01 -0400

    create function to populate user structs with names and phone numbers and map to names list

--- go/learn_go/maps/maps_intro.go
+++ go/learn_go/maps/maps_intro.go
@@ -1,9 +1,18 @@
 package main
 
-import "errors"
+import (
+	"errors"
+)
 
 func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
-	// ?
+	if len(names) != len(phoneNumbers) {
+		return nil, errors.New("invalid sizes")
+	}
+	users := make(map[string]user)
+	for i, key := range names {
+		users[key] = user{name: key, phoneNumber: phoneNumbers[i]}
+	}
+	return users, nil
 }
 
 type user struct {
