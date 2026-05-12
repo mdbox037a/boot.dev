83b17c81de91044311d4c821740199378d9d2c88
Matt Box <mdbox037a@protonmail.com>
2026-05-11 20:07:49 -0400

    complete logic to tag messages with promo or urgent

--- go/learn_go/slices/message_tagger.go
+++ go/learn_go/slices/message_tagger.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type sms struct {
 	id      string
 	content string
@@ -7,10 +9,20 @@ type sms struct {
 }
 
 func tagMessages(messages []sms, tagger func(sms) []string) []sms {
-	// ?
+	for i, message := range messages {
+		tagResults := tagger(message)
+		messages[i].tags = tagResults
+	}
+	return messages
 }
 
 func tagger(msg sms) []string {
 	tags := []string{}
-	// ?
+	if strings.Contains(strings.ToLower(msg.content), "urgent") {
+		tags = append(tags, "Urgent")
+	}
+	if strings.Contains(strings.ToLower(msg.content), "sale") {
+		tags = append(tags, "Promo")
+	}
+	return tags
 }
