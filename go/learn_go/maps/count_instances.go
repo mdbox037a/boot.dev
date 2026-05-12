224b3c9912ba97b719ebd41402f715ac4d8e490f
Matt Box <mdbox037a@protonmail.com>
2026-05-11 20:48:17 -0400

    complete logic to update message counts for valid users

--- go/learn_go/maps/count_instances.go
+++ go/learn_go/maps/count_instances.go
@@ -1,5 +1,10 @@
 package main
 
 func updateCounts(messagedUsers []string, validUsers map[string]int) {
-	// ?
+	for _, user := range messagedUsers {
+		_, ok := validUsers[user]
+		if ok {
+			validUsers[user] += 1
+		}
+	}
 }
