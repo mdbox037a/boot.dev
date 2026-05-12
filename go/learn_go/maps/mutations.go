8155dd8356373ec3ff934977f34a96638c04c382
Matt Box <mdbox037a@protonmail.com>
2026-05-11 20:32:11 -0400

    complete logic to check if user is marked for deletion and execute if necessary

--- go/learn_go/maps/mutations.go
+++ go/learn_go/maps/mutations.go
@@ -1,7 +1,17 @@
 package main
 
+import "errors"
+
 func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
-	// ?
+	temp, ok := users[name]
+	if !ok {
+		return false, errors.New("not found")
+	} else if !temp.scheduledForDeletion {
+		return false, nil
+	} else {
+		delete(users, name)
+		return true, nil
+	}
 }
 
 type user struct {
