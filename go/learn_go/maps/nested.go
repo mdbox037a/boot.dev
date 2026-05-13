d8e77266c07454f30dff4e225304b676a8f7b01e
Matt Box <mdbox037a@protonmail.com>
2026-05-12 21:33:47 -0400

    complete first working draft of logic for getNameCounts function

--- go/learn_go/maps/nested.go
+++ go/learn_go/maps/nested.go
@@ -1,5 +1,17 @@
 package main
 
 func getNameCounts(names []string) map[rune]map[string]int {
-	// Your code here
+	nestedMap := map[rune]map[string]int{}
+	for _, name := range names {
+		runes := []rune(name)
+		letter := runes[0]
+		if section, ok := nestedMap[letter]; ok {
+			section[name]++
+		} else {
+			section = map[string]int{}
+			nestedMap[letter] = section
+			section[name]++
+		}
+	}
+	return nestedMap
 }
