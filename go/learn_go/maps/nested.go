038f49c231712223dc43746903cd60b81d0a7f20
Matt Box <mdbox037a@protonmail.com>
2026-05-12 21:37:36 -0400

    refactor for DRY and better use of make to initialize empty maps

--- go/learn_go/maps/nested.go
+++ go/learn_go/maps/nested.go
@@ -1,17 +1,13 @@
 package main
 
 func getNameCounts(names []string) map[rune]map[string]int {
-	nestedMap := map[rune]map[string]int{}
+	nameCounts := make(map[rune]map[string]int)
 	for _, name := range names {
-		runes := []rune(name)
-		letter := runes[0]
-		if section, ok := nestedMap[letter]; ok {
-			section[name]++
-		} else {
-			section = map[string]int{}
-			nestedMap[letter] = section
-			section[name]++
+		firstLetter := []rune(name)[0]
+		if _, ok := nameCounts[firstLetter]; !ok {
+			nameCounts[firstLetter] = make(map[string]int)
 		}
+		nameCounts[firstLetter][name]++
 	}
-	return nestedMap
+	return nameCounts
 }
