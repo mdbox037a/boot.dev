3e1d84f44219aa271b40017aa5ad30dbcbf73eb1
Matt Box <mdbox037a@protonmail.com>
2026-05-13 21:54:54 -0400

    refactor to make distinct words counter more concise

--- go/learn_go/maps/distinct_words.go
+++ go/learn_go/maps/distinct_words.go
@@ -5,11 +5,8 @@ import "strings"
 func countDistinctWords(messages []string) int {
 	distinctWords := make(map[string]struct{})
 	for _, msg := range messages {
-		for _, word := range strings.Fields(msg) {
-			lower_word := strings.ToLower(word)
-			if _, ok := distinctWords[lower_word]; !ok {
-				distinctWords[lower_word] = struct{}{}
-			}
+		for _, word := range strings.Fields(strings.ToLower(msg)) {
+			distinctWords[word] = struct{}{}
 		}
 	}
 	return len(distinctWords)
