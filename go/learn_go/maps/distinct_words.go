dfbf6acce1759cf1825b229b14cf158c920471c6
Matt Box <mdbox037a@protonmail.com>
2026-05-13 21:53:21 -0400

    complete initial draft of word count logic

--- go/learn_go/maps/distinct_words.go
+++ go/learn_go/maps/distinct_words.go
@@ -1,5 +1,16 @@
 package main
 
+import "strings"
+
 func countDistinctWords(messages []string) int {
-	// ?
+	distinctWords := make(map[string]struct{})
+	for _, msg := range messages {
+		for _, word := range strings.Fields(msg) {
+			lower_word := strings.ToLower(word)
+			if _, ok := distinctWords[lower_word]; !ok {
+				distinctWords[lower_word] = struct{}{}
+			}
+		}
+	}
+	return len(distinctWords)
 }
