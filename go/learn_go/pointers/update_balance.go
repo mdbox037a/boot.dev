d4ed41b15ad8f1b236181d4549b69ed8af77050c
Matt Box <mdbox037a@protonmail.com>
2026-05-16 15:57:22 -0400

    complete function to add or subtract from cx balance per transaction

--- go/learn_go/pointers/update_balance.go
+++ go/learn_go/pointers/update_balance.go
@@ -21,3 +21,18 @@ type transaction struct {
 	amount          float64
 	transactionType transactionType
 }
+
+func updateBalance(cx *customer, tx transaction) error {
+	if tx.transactionType == transactionDeposit {
+		cx.balance += tx.amount
+		return nil
+	}
+	if tx.transactionType == transactionWithdrawal {
+		if cx.balance-tx.amount < 0 {
+			return errors.New("insufficient funds")
+		}
+		cx.balance -= tx.amount
+		return nil
+	}
+	return errors.New("unknown transaction type")
+}
