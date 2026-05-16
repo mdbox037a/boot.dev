19d8b8bf4ebdafd4ef035368f4740f785eae37b7
Matt Box <mdbox037a@protonmail.com>
2026-05-16 15:47:44 -0400

    set up structs and constants for balance update function (todo)

---
+++ go/learn_go/pointers/update_balance.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+)
+
+type customer struct {
+	id      int
+	balance float64
+}
+
+type transactionType string
+
+const (
+	transactionDeposit    transactionType = "deposit"
+	transactionWithdrawal transactionType = "withdrawal"
+)
+
+type transaction struct {
+	customerID      int
+	amount          float64
+	transactionType transactionType
+}
