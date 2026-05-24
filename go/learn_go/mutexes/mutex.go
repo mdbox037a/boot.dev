8aec13f31761203524c2b06c7cfbc27e05500c6c
Matt Box <mdbox037a@protonmail.com>
2026-05-24 16:07:45 -0400

    set up troubleshooting file for first mutexes exercise

---
+++ go/learn_go/mutexes/mutex.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+type safeCounter struct {
+	counts map[string]int
+	mu     *sync.Mutex
+}
+
+func (sc safeCounter) inc(key string) {
+	// ?
+	sc.slowIncrement(key)
+}
+
+func (sc safeCounter) val(key string) int {
+	// ?
+	return sc.slowVal(key)
+}
+
+func (sc safeCounter) slowIncrement(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
+
+func (sc safeCounter) slowVal(key string) int {
+	time.Sleep(time.Microsecond)
+	return sc.counts[key]
+}
