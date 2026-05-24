32489aebc59ef4a1ab9cb4f018fe51454cdcd7de
Matt Box <mdbox037a@protonmail.com>
2026-05-24 15:15:51 -0400

    create email queue and logic to store each in queue before returning channel

--- go/learn_go/channels/buffered_channels.go
+++ go/learn_go/channels/buffered_channels.go
@@ -1,5 +1,9 @@
 package main
 
 func addEmailsToQueue(emails []string) chan string {
-	// ?
+	emailQueue := make(chan string, len(emails))
+	for _, email := range emails {
+		emailQueue <- email
+	}
+	return emailQueue
 }
