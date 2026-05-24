94e0d47bc0e452d97876f4aecbce5c7a7f97b842
Matt Box <mdbox037a@protonmail.com>
2026-05-24 15:24:41 -0400

    set up function signature for concurrentFibonacci channel example

---
+++ go/learn_go/channels/range.go
@@ -0,0 +1,14 @@
+package main
+
+func concurrentFib(n int) []int {
+	// ?
+}
+
+func fibonacci(n int, ch chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	close(ch)
+}
