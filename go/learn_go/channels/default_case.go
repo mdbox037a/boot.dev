026fb322b5b83bd2e27491e19db17253cb9c8fa0
Matt Box <mdbox037a@protonmail.com>
2026-05-24 15:38:40 -0400

    set up saveBackups function signature and helper functions

---
+++ go/learn_go/channels/default_case.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"time"
+)
+
+func saveBackups(snapshotTicker, saveAfter <-chan time.Time, logChan chan string) {
+	// ?
+}
+
+func takeSnapshot(logChan chan string) {
+	logChan <- "Taking a backup snapshot..."
+}
+
+func saveSnapshot(logChan chan string) {
+	logChan <- "All backups saved!"
+	close(logChan)
+}
+
+func waitForData(logChan chan string) {
+	logChan <- "Nothing to do, waiting..."
+}
