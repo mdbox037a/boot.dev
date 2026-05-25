fc0f5b5dab2a135afdf970c552a90f5a9805ccd0
Matt Box <mdbox037a@protonmail.com>
2026-05-25 07:55:43 -0400

    create skeleton file to begin json parsing module

---
+++ go/http_clients/decoding_json.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func getIssues(url string) ([]Issue, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+
+	// ?
+}
