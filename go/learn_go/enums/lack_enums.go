c2960def18aedd61ed134890b6b08683519aa8f5
Matt Box <mdbox037a@protonmail.com>
2026-05-25 07:03:12 -0400

    add error handling to both function calls

--- go/learn_go/enums/lack_enums.go
+++ go/learn_go/enums/lack_enums.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func (a *analytics) handleEmailBounce(em email) error {
-	em.recipient.updateStatus(em.status)
-	a.track(em.status)
+	var err error
+
+	err = em.recipient.updateStatus(em.status)
+	if err != nil {
+		return fmt.Errorf("error updating user status: %w", err)
+	}
+	err = a.track(em.status)
+	if err != nil {
+		return fmt.Errorf("error tracking user bounce: %w", err)
+	}
 	return nil
 }
