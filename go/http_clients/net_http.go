fb6e8c5991ca97e87663cb561d882c8d9724859c
Matt Box <mdbox037a@protonmail.com>
2026-05-25 07:30:49 -0400

    create buggy client function for troubleshooting practice

---
+++ go/http_clients/net_http.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func getIssueData(url string) ([]byte, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+	return res, nil
+}
