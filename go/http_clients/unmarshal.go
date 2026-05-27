7d1b58fa4f2ebed76454c1af5762638f64c1623f
Matt Box <mdbox037a@protonmail.com>
2026-05-26 20:43:05 -0400

    complete logic to unmarshal response body into byte slice

--- go/http_clients/unmarshal.go
+++ go/http_clients/unmarshal.go
@@ -7,12 +7,21 @@ import (
 	"net/http"
 )
 
-func getIssueData(url string) ([]byte, error) {
+func getIssues(url string) ([]Issue, error) {
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)
 	}
 	defer res.Body.Close()
 
-	// ?
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var issues []Issue
+	if err := json.Unmarshal(data, &issues); err != nil {
+		return nil, err
+	}
+	return issues, err
 }
