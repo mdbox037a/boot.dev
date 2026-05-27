111b84b1e1b100ed5d7dab6eaf4fa386a9a91826
Matt Box <mdbox037a@protonmail.com>
2026-05-26 21:45:06 -0400

    complete logic to decode and parse maps

--- go/http_clients/map_string_interface.go
+++ go/http_clients/map_string_interface.go
@@ -17,14 +17,22 @@ func getResources(url string) ([]map[string]any, error) {
 
 	defer res.Body.Close()
 
-	// ?
-
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&resources); err != nil {
+		return nil, err
+	}
+	return resources, nil
 }
 
 func logResources(resources []map[string]any) {
 	var formattedStrings []string
 
-	// ?
+	for _, m := range resources {
+		for key, value := range m {
+			temp := fmt.Sprintf("Key: %s - Value: %v", key, value)
+			formattedStrings = append(formattedStrings, temp)
+		}
+	}
 
 	sort.Strings(formattedStrings)
 
