2a96f7e40865b4acdcccda4e8afdf6c12fde5201
Matt Box <128308984+mdbox037a@users.noreply.github.com>
2026-05-28 22:20:33 -0400

    Update reference_vs_value.py
    Naming change for clarity

--- reference_vs_value.py
+++ reference_vs_value.py
@@ -1,10 +1,10 @@
 def add_format(default_formats, new_format):
-    new_dictionary = default_formats.copy()
-    new_dictionary[new_format] = True
-    return new_dictionary
+    my_dictionary = default_formats.copy()
+    my_dictionary[new_format] = True
+    return my_dictionary
 
 
 def remove_format(default_formats, old_format):
-    new_dictionary = default_formats.copy()
-    new_dictionary[old_format] = False
-    return new_dictionary
+    my_dictionary = default_formats.copy()
+    my_dictionary[old_format] = False
+    return my_dictionary
