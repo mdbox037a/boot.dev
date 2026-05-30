d9054c45932333ffdf25b16a676859ceea12ad4a
Matt Box <mdbox037a@protonmail.com>
2026-05-30 16:05:13 -0400

    complete logic to create client and post to endpoint

--- go/http_clients/methods/post.go
+++ go/http_clients/methods/post.go
@@ -7,5 +7,30 @@ import (
 )
 
 func createUser(url, apiKey string, data User) (User, error) {
-	// ?
+	userJsonData, err := json.Marshal(data)
+	if err != nil {
+		return User{}, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(userJsonData))
+	if err != nil {
+		return User{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+	defer res.Body.Close()
+
+	var user User
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&user); err != nil {
+		return User{}, err
+	}
+	return user, nil
 }
