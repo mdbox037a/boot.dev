ad955b8669129feeb0e6579427ad94777ca2bcbb
Matt Box <mdbox037a@protonmail.com>
2026-05-30 15:51:42 -0400

    complete logic to decode get request body and stream into user struct

--- go/http_clients/methods/get.go
+++ go/http_clients/methods/get.go
@@ -6,5 +6,18 @@ import (
 )
 
 func getUsers(url string) ([]User, error) {
-	// ?
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	users := []User{}
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&users); err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
