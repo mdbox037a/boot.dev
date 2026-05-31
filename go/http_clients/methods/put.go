a47e98bf36409e41edd65051744ce9b852894782
Matt Box <mdbox037a@protonmail.com>
2026-05-31 19:54:52 -0400

    complete first draft of updateUser logic

--- go/http_clients/methods/put.go
+++ go/http_clients/methods/put.go
@@ -9,7 +9,32 @@ import (
 func updateUser(baseURL, id, apiKey string, data User) (User, error) {
 	fullURL := baseURL + "/" + id
 
-	// ?
+	userJsonData, err := json.Marshal(data)
+	if err != nil {
+		return User{}, err
+	}
+
+	req, err := http.NewRequest("PUT", fullURL, bytes.NewBuffer(userJsonData))
+	if err != nil {
+		return User{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := &http.Client
+	res, err := client.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+
+	var user User
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&user); err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
 
 func getUserById(baseURL, id, apiKey string) (User, error) {
