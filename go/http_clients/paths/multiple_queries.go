bc5863c0589c7e72c9dd2980a369e43b310e9c6f
Matt Box <mdbox037a@protonmail.com>
2026-05-31 20:36:11 -0400

    complete logic to build multiple query params in fullURL

--- go/http_clients/paths/multiple_queries.go
+++ go/http_clients/paths/multiple_queries.go
@@ -1,8 +1,19 @@
 package main
 
 func fetchTasks(baseURL, availability string) []Issue {
-	// ?
+	sort := "estimate"
+	var limit string
+	switch availability {
+	case "Low":
+		limit = "1"
+	case "Medium":
+		limit = "3"
+	case "High":
+		limit = "5"
+	default:
+		limit = "0"
+	}
 
-	fullURL := baseURL
+	fullURL := baseURL + "?sort=" + sort + "&limit=" + limit
 	return getIssues(fullURL)
 }
