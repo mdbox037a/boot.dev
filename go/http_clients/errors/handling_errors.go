312164ff32187434529b9b1a1b48f04b25b79e1a
Matt Box <mdbox037a@protonmail.com>
2026-05-31 20:49:02 -0400

    complete function to handle network errors and non-ok http statuses

--- go/http_clients/errors/handling_errors.go
+++ go/http_clients/errors/handling_errors.go
@@ -6,5 +6,14 @@ import (
 )
 
 func fetchData(url string) (int, error) {
-	// ?
+	res, err := http.Get(url)
+	if err != nil {
+		return 0, fmt.Errorf("network error: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return res.StatusCode, fmt.Errorf("non-OK HTTP status: %s", res.Status)
+	}
+	return res.StatusCode, nil
 }
