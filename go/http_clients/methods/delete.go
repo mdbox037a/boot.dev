53f04bbc02f33bcda28beee0f4d835a961e0a175
Matt Box <mdbox037a@protonmail.com>
2026-05-31 20:11:59 -0400

    complete logic to send delete requests to server

--- go/http_clients/methods/delete.go
+++ go/http_clients/methods/delete.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
 )
 
 func deleteUser(baseURL, id, apiKey string) error {
 	fullURL := baseURL + "/" + id
 
-	// ?
+	req, err := http.NewRequest("DELETE", fullURL, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode > 299 {
+		return errors.New("request to delete item unsuccessful")
+	}
+	return nil
 }
