2057ab3fb07b232ba07240be11881a8b4cbe5b37
Matt Box <mdbox037a@protonmail.com>
2026-06-06 21:27:56 -0400

    create small query to test untyped SQLite system

---
+++ sql/introduction/comparing.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
+
+
+INSERT INTO
+  users (id, name, age)
+VALUES
+  (1, 'John Doe', 21);
+
+
+INSERT INTO
+  users (id, name, age)
+VALUES
+  (2, 'Montgomery Burns', 33);
+
+
+SELECT
+  *
+FROM
+  users;
