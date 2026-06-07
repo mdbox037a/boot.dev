04d873a557330bad13d71a07cf21393c99f70f40
Matt Box <mdbox037a@protonmail.com>
2026-06-07 15:34:31 -0400

    complete first exercise with auto-increment of user ID on insert

---
+++ sql/crud/auto_increment.sql
@@ -0,0 +1,26 @@
+INSERT INTO
+  users (
+    name,
+    age,
+    country_code,
+    username,
+    password,
+    is_admin
+  )
+VALUES
+  (
+    'Lance',
+    20,
+    'US',
+    'LanChr',
+    'bootdevisbest',
+    false
+  ),
+  (
+    'Tiffany',
+    28,
+    'US',
+    'Tifferoon',
+    'autoincrement',
+    true
+  );
