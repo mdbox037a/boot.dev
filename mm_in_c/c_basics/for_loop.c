e662aac7b028405204283a216530ec0d2bfb71e6
Matt Box <128308984+mdbox037a@users.noreply.github.com>
2026-06-14 22:13:08 -0400

    Update for_loop.c

--- mm_in_c/c_basics/for_loop.c
+++ mm_in_c/c_basics/for_loop.c
@@ -6,3 +6,10 @@ void print_numbers(int start, int end) {
 	for (i = start; i <= end; i++)
 		printf("%d\n", i);
 }
+
+void print_evens(int start, int end) {
+    int i;
+    
+    for (i = start; i <= end; i++)
+        printf("%d\n", 2 * i);
+}
